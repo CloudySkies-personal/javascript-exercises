d753882f8cc8e1ec0209249aae9a77cd776c4ab6
James <work.jtempleton@gmail.com>
2025-01-02 02:08:55 +0800

    Accomplish up to task 3

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,7 +1,7 @@
 const sumAll = function(num1, num2) {
     /*This function solves for the sum of all integers betweeen two given numbers
      using Gaussian Summation*/
-    gaussianSum = (num1+num2)*((num2-num1+1)/2)
+    gaussianSum = (num1+num2)*((Math.abs(num2-num1)+1)/2)
     return gaussianSum
 };
 
