444696719361833f035f58aac19330a3e5dc48a1
James <work.jtempleton@gmail.com>
2025-01-02 02:21:59 +0800

    Accomplish up to task 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,7 @@
 const sumAll = function(num1, num2) {
-    if (num1 < 0 || num2 <0){
+    console.log(`${num1}: ${typeof num1}, ${num2}:${typeof num2}`)
+    if ((num1 < 0 || num2 <0) 
+        || (!Number.isInteger(num1) || !Number.isInteger(num2))){
         return ("ERROR")
     }
     /*This function solves for the sum of all integers betweeen two given numbers
@@ -8,5 +10,6 @@ const sumAll = function(num1, num2) {
     return gaussianSum
 };
 
+sumAll()
 // Do not edit below this line
 module.exports = sumAll;
