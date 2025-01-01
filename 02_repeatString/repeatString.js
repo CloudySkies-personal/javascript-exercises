d91dac17d8c858130b33a9bf9d56b82d39b91cc3
James <work.jtempleton@gmail.com>
2025-01-02 01:02:03 +0800

    Accomplish all tasks

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
 
+const repeatString = function(string, num) {
+   if (num < 0){
+    let final_string = "ERROR"
+    return (final_string)
+   }
+   let final_string = ""
+   for (i=0; i<num; i++){
+    final_string += string;
+   };
+
+   return (final_string)
 };
 
 // Do not edit below this line
