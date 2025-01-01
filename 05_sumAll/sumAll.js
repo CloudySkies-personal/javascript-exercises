252663e40ddf6df0d091f01792b6e767400be3a0
James <work.jtempleton@gmail.com>
2025-01-02 02:10:53 +0800

    Accomplish up to task 4

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,7 @@
 const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 <0){
+        return ("ERROR")
+    }
     /*This function solves for the sum of all integers betweeen two given numbers
      using Gaussian Summation*/
     gaussianSum = (num1+num2)*((Math.abs(num2-num1)+1)/2)
