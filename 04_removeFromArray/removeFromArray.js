2aab52691838811e37b5d12852e14e3727c56472
James <work.jtempleton@gmail.com>
2025-01-02 01:31:17 +0800

    Accomplish tasks 1 and 2

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...numbers) {
+    for (number of numbers){
+        const index = array.indexOf(number)
+        array.splice(index,1)
+    };
+    return (array)
 };
-
+console.log(removeFromArray([1,2,3], 3))
 // Do not edit below this line
 module.exports = removeFromArray;
