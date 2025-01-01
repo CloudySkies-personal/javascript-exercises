e4edc289e6b8e2f1e5fc248fc51830332a50d228
James <work.jtempleton@gmail.com>
2025-01-02 01:15:04 +0800

    Accomplish task 1

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let stringToArray = string.split("");
+    let reverseArray = stringToArray.reverse();
+    let stringReversed = reverseArray.join("");
+    return stringReversed
 };
 
 // Do not edit below this line
