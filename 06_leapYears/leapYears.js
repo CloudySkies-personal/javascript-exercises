ce90bfe2fd154b6f17bcbc37173a31a74a5c78a6
James <work.jtempleton@gmail.com>
2025-01-02 22:14:41 +0800

    Accomplish all tasks

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -2,9 +2,11 @@ const leapYears = function(year) {
     if (year % 4 === 0){
         if ((year % 100 === 0) && (year % 400!==0)){
             return false;
+        } else {
+            return true;
         }        
     } else {
-        return true;
+        return false;
     }
 };
 
