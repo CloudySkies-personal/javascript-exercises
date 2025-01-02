7f854855ac4e233ff02d0619aa74c480223c26a9
James <work.jtempleton@gmail.com>
2025-01-02 22:24:56 +0800

    First commit

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const celToFahrFixedConstant = 9/5
+const fahrToCelFixedConstant = (1/(9/5))
+
+const convertToCelsius = function(tempToConvert) {
+    return (tempToConvert - 32) * (fahrToCelFixedConstant)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempToConvert) {
+    return ((celToFahrFixedConstant * tempToConvert) + 32) 
 };
 
 // Do not edit below this line
